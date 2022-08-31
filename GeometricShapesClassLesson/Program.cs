821ea98f63b10b3e1c81a96a2d260f1bfa671f49
Sam Schultz <sam.schultz@outlook.com>
2022-08-31 11:45:49 -0400

    Add project files.

---
+++ GeometricShapesClassLesson/Program.cs
@@ -0,0 +1,11 @@
+﻿using GeometricShapesClassLesson;
+using GeometricShapesClassLesson;
+
+Quad q1 = new(3, 4, 5, 6);
+Console.WriteLine($"Perimeter is {q1.Perimeter()}");
+
+Rect r1 = new(3, 5);
+Console.WriteLine($"The Area is {r1.Area()} and The Perimeter is {r1.Perimeter()}");
+
+Square s1 = new(5);
+Console.WriteLine($"The Area is {s1.Area()} and The Perimeter is {s1.Perimeter()}");
---
+++ GeometricShapesClassLesson/Square.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometricShapesClassLesson
+{
+    internal class Square
+    {
+        public int Side1 { get; set; }
+
+        public int Perimeter()
+        {
+            return Side1 * 4;
+        }
+
+        public int Area()
+        {
+            return Side1 * Side1;
+        }
+
+        public Square(int side)
+        {
+            Side1 = side;
+        }
+
+
+
+    }
+}
